7170d79edad1ebdd83baef8a9b937c81544c7304
kubra-sen <31575542+kubra-sen@users.noreply.github.com>
2021-09-22 16:50:48 +0300

    Update HybridRecommenderSystem.py

--- HybridRecommenderSystem.py
+++ HybridRecommenderSystem.py
@@ -20,9 +20,8 @@ def most_similar_user(df, user_id, common_movie_threshold, corr_threshold):
     user_watched_df = df[df.index == user_id]
     movies_watched = user_watched_df.columns[user_watched_df.notna().any()].tolist()
     movies_watched_df = df[movies_watched]
+    
     # Finding the users who watched the same movies at least with a given number
-
-    movies_watched_df = df[movies_watched]
     user_movie_count = movies_watched_df.T.notnull().sum()
     user_movie_count = user_movie_count.reset_index()
     user_movie_count.columns = ["userId", "movie_count"]
